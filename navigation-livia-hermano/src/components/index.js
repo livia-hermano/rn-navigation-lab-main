1d9a72a8c4f2e312b71e19125b9d5443ec131f04
livia-hermano <livia.hermano@aluno.senai.br>
2026-05-30 15:55:56 -0300

    index finalizado

--- navigation-livia-hermano/src/components/index.js
+++ navigation-livia-hermano/src/components/index.js
@@ -1,6 +1,8 @@
 // Exportacoes centralizadas dos componentes (barrel export)
 // Importar de um unico lugar: import { BotaoAcao } from '../components';
+import {BotaoAcao} from '../components'
 export { default as BotaoAcao } from './BotaoAcao';
 
 // TODO: apos criar CardJogo.js, adicione a linha abaixo:
 // export { default as CardJogo } from './CardJogo';
+export {default as CardJogo} from './CardJogo'

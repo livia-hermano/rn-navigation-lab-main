43c726fd31462acdcc90b3aa70ab6554ff4334af
livia-hermano <livia.hermano@aluno.senai.br>
2026-05-30 15:53:40 -0300

    estilizacao correta do cardjogos

--- navigation-livia-hermano/src/components/CardJogo.js
+++ navigation-livia-hermano/src/components/CardJogo.js
@@ -3,11 +3,29 @@ import { Text, View } from "react-native";
 // Passe os parametros de forma correta e realize a estilização do componente
 export default function CardJogo() {
   return (
-    <View>
-      <Text>Titulo</Text>
-      <Text>Genero</Text>
-      <Text>Plataforma</Text>
-      <Text>Nota</Text>
+    <View style={styles.card}>
+      <Text style={styles.titulo}>Titulo</Text>
+      <Text style={styles.text}>Genero</Text>
+      <Text style={styles.text}>Plataforma</Text>
+      <Text style={styles.text}>Nota</Text>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  card: {
+    backgorundColor: "#ffff".
+    borderRadius: 10,
+    padding: 16,
+    borderWidth: 1,
+    borderColor: "#2c2e39",
+  },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  text: {
+    fontSize: 10,
+  }
+})
